a9b55bc3848781185e6845f784bddb5e08b202dc
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-06-27 12:36:49 +0800

    [13] Update by adding richtext

--- client/lib/feature/auth/view/pages/signup_page.dart
+++ client/lib/feature/auth/view/pages/signup_page.dart
@@ -14,32 +14,42 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Padding(
-        padding: EdgeInsets.all(15.0),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Sign Up.',
               style: TextStyle(
                 fontSize: 50,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 30),
-            CustomField(
+            const SizedBox(height: 30),
+            const CustomField(
               hintText: 'Name',
             ),
-            SizedBox(height: 15),
-            CustomField(
+            const SizedBox(height: 15),
+            const CustomField(
               hintText: 'Email',
             ),
-            SizedBox(height: 15),
-            CustomField(
+            const SizedBox(height: 15),
+            const CustomField(
               hintText: 'Password',
             ),
-            SizedBox(height: 20),
-            AuthGradientButton(),
+            const SizedBox(height: 20),
+            const AuthGradientButton(),
+            const SizedBox(height: 20),
+            RichText(
+              text: TextSpan(
+                text: 'Already have an account?',
+                style: Theme.of(context).textTheme.titleMedium,
+                children: const [
+                  TextSpan(text: ' Sign In'),
+                ],
+              ),
+            )
           ],
         ),
       ),
