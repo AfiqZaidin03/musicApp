256b81c9b44fb8f5bbbde6971a00a00fc5bb99c7
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-06-27 12:16:41 +0800

    [9] Extract OutlineBorder

--- client/lib/core/theme/theme.dart
+++ client/lib/core/theme/theme.dart
@@ -2,24 +2,20 @@ import 'package:client/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static OutlineInputBorder _border(Color color) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+
   static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: Pallete.backgroundColor,
     inputDecorationTheme: InputDecorationTheme(
       contentPadding: const EdgeInsets.all(27),
-      enabledBorder: OutlineInputBorder(
-        borderSide: const BorderSide(
-          color: Pallete.borderColor,
-          width: 3,
-        ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: const BorderSide(
-          color: Pallete.gradient2,
-          width: 3,
-        ),
-        borderRadius: BorderRadius.circular(10),
-      ),
+      enabledBorder: _border(Pallete.borderColor),
+      focusedBorder: _border(Pallete.gradient2),
     ),
   );
 }
