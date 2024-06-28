5857b6b72ee0cb56d17d4bd4719022c1f57e5572
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-06-28 15:38:26 +0800

    [29] Create login route and status

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -8,8 +8,11 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
+from pydantic_schemas.user_login import UserLogin
 
 router = APIRouter()
+
+# Signup
 
 
 @router.post('/signup')
@@ -30,5 +33,24 @@ def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+
+    return user_db
+
+# Login
+
+
+@router.post('/login')
+def login_user(user: UserLogin, db: Session = Depends(get_db)):
+    # check if the user with same email already exists
+    user_db = db.query(User).filter(User.email == user.email).first()
+
+    if not user_db:
+        raise HTTPException(400, 'User with this email does not exist!')
+
+    # password matching or not
+    is_match = bcrypt.checkpw(user.password.encode(), user_db.password)
+
+    if not is_match:
+        raise HTTPException(400, 'Incorrect password!')
 
     return user_db
