bf87b3da6273e83bd80e54d5c4836e4ce8ff89f2
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-05 22:11:28 +0800

    [68] Add authLocalRepository to login function

--- client/lib/feature/auth/viewmodel/auth_viewmodel.dart
+++ client/lib/feature/auth/viewmodel/auth_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:client/feature/auth/model/user_model.dart';
+import 'package:client/feature/auth/repositories/auth_local_repository.dart';
 import 'package:client/feature/auth/repositories/auth_remote_repository.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -8,10 +9,12 @@ part 'auth_viewmodel.g.dart';
 @riverpod
 class AuthViewModel extends _$AuthViewModel {
   late AuthRemoteRepository _authRemoteRepository;
+  late AuthLocalRepository _authLocalRepository;
 
   @override
   AsyncValue<UserModel>? build() {
     _authRemoteRepository = ref.watch(authRemoteRepositoryProvider);
+    _authLocalRepository = ref.watch(authLocalRepositoryProvider);
     return null;
   }
 
@@ -48,8 +51,13 @@ class AuthViewModel extends _$AuthViewModel {
     final val = switch (res) {
       Left(value: final l) => state =
           AsyncValue.error(l.message, StackTrace.current),
-      Right(value: final r) => state = AsyncValue.data(r),
+      Right(value: final r) => _loginSuccess(r),
     };
     print(val);
+  }
+
+  AsyncValue<UserModel>? _loginSuccess(UserModel user) {
+    _authLocalRepository.setToken(user.token);
+    return state = AsyncValue.data(user);
   }
 }
