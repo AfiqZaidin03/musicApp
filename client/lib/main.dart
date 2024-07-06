d3f12b479efc37287613e9f80fb30b9384255697
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-06 15:53:01 +0800

    [78] Change title of app

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Music App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkThemeMode,
       home: const SignupPage(),
