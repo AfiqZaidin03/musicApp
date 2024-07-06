6169171274fe301e1322ba70b6f0cb2ebd3a8bbc
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-06 16:36:08 +0800

    [85] Add UploadSongPage to main to display the page and add appbar title with body content pick thumbnail song

--- client/lib/feature/home/view/pages/upload_song_page.dart
+++ client/lib/feature/home/view/pages/upload_song_page.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,6 +15,31 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Upload Song'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          DottedBorder(
+            child: const SizedBox(
+              height: 150,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.folder_open,
+                    size: 40,
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Select the thumbnail for your song',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
