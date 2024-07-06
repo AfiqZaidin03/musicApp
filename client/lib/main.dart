6febf009cb1d5f72f30152fcea926ce7b811194d
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-06 16:11:31 +0800

    [81] Add currentUser variable to display homepage after user login and create homepage

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:client/core/provider/current_user_notifier.dart';
 import 'package:client/core/theme/theme.dart';
+import 'package:client/feature/auth/pages/home_page.dart';
 import 'package:client/feature/auth/view/pages/signup_page.dart';
 import 'package:client/feature/auth/viewmodel/auth_viewmodel.dart';
 import 'package:flutter/material.dart';
@@ -8,9 +10,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final container = ProviderContainer();
   await container.read(authViewModelProvider.notifier).initSharedPreferences();
-  final userModel =
-      await container.read(authViewModelProvider.notifier).getData();
-  print(userModel);
+  await container.read(authViewModelProvider.notifier).getData();
+
   runApp(
     UncontrolledProviderScope(
       container: container,
@@ -19,16 +20,18 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserNotifierProvider);
+
     return MaterialApp(
       title: 'Music App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkThemeMode,
-      home: const SignupPage(),
+      home: currentUser == null ? const SignupPage() : const HomePage(),
     );
   }
 }
