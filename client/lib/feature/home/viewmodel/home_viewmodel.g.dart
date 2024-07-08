4342594d3c099a336709904373a97f4ffb6e591a
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-08 22:15:28 +0800

    [119] Create home_viewmodel

---
+++ client/lib/feature/home/viewmodel/home_viewmodel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$homeViewmodelHash() => r'f1e2ad85a3f2797323d847b80bdf129dd0044a9d';
+
+/// See also [HomeViewmodel].
+@ProviderFor(HomeViewmodel)
+final homeViewmodelProvider =
+    AutoDisposeNotifierProvider<HomeViewmodel, AsyncValue?>.internal(
+  HomeViewmodel.new,
+  name: r'homeViewmodelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$homeViewmodelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$HomeViewmodel = AutoDisposeNotifier<AsyncValue?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
