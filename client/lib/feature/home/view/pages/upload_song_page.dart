50aba6210effb19e795f723d041fe661e5b368ec
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-08 14:44:22 +0800

    [100] Update pick image function and styling for image selected

--- client/lib/feature/home/view/pages/upload_song_page.dart
+++ client/lib/feature/home/view/pages/upload_song_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/utils.dart';
 import 'package:client/core/widgets/custom_field.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
@@ -16,6 +19,18 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
   final songNameController = TextEditingController();
   final artistController = TextEditingController();
   Color selectedColor = Pallete.cardColor;
+  File? seletedImage;
+  File? seletedAudio;
+
+  void selectAudio() {}
+  void selectImage() async {
+    final pickedImage = await pickImage();
+    if (pickedImage != null) {
+      setState(() {
+        seletedImage = pickedImage;
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -42,30 +57,41 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              DottedBorder(
-                color: Pallete.borderColor,
-                radius: const Radius.circular(10),
-                borderType: BorderType.RRect,
-                dashPattern: const [10, 4],
-                strokeCap: StrokeCap.round,
-                child: const SizedBox(
-                  height: 150,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
+              GestureDetector(
+                onTap: selectImage,
+                child: seletedImage != null
+                    ? SizedBox(
+                        height: 150,
+                        width: double.infinity,
+                        child: Image.file(
+                          seletedImage!,
+                          fit: BoxFit.cover,
+                        ))
+                    : DottedBorder(
+                        color: Pallete.borderColor,
+                        radius: const Radius.circular(10),
+                        borderType: BorderType.RRect,
+                        dashPattern: const [10, 4],
+                        strokeCap: StrokeCap.round,
+                        child: const SizedBox(
+                          height: 150,
+                          width: double.infinity,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(height: 15),
+                              Text(
+                                'Select the thumbnail for your song',
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      SizedBox(height: 15),
-                      Text(
-                        'Select the thumbnail for your song',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
-                  ),
-                ),
               ),
               const SizedBox(height: 40),
               CustomField(
