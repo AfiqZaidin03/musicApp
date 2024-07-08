db626a1df585714f90e9529a2645484869102a85
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-08 16:42:38 +0800

    [114] Song api for upload

---
+++ server/routes/song.py
@@ -0,0 +1,45 @@
+import os
+import uuid
+
+import cloudinary
+import cloudinary.uploader
+from dotenv import load_dotenv
+from fastapi import APIRouter, Depends, File, Form, UploadFile
+from sqlalchemy.orm import Session
+
+from database import get_db
+from middleware.auth_middleware import auth_middleware
+
+router = APIRouter()
+
+load_dotenv()
+
+cloudinary.config(
+    cloud_name="dmktlpvmm",
+    api_key="512273844721696",
+    # Click 'View Credentials' below to copy your API secret
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
+
+
+@router.post('/upload')
+def upload_song(song: UploadFile = File(...),
+                thumbnail: UploadFile = File(...),
+                artist: str = Form(...),
+                song_name: str = Form(...),
+                hex_code: str = Form(...),
+                db: Session = Depends(get_db),
+                auth_dict=Depends(auth_middleware)):
+    song_id = str(uuid.uuid4())
+    song_res = cloudinary.uploader.upload(
+        song.file, resource_type='auto', folder=f'songs/{song_id}')
+    print(song_res)
+
+    thumbnail_res = cloudinary.uploader.upload(
+        thumbnail.file, resource_type='image', folder=f'songs/{song_id}')
+    print(thumbnail_res)
+
+    # store data in db
+
+    return 'ok'
