f41243ef94923876f4dbc13001a42ae2e1a650f6
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-09 14:43:36 +0800

    [130] Add 2 bottomNavigationBarItem

--- client/lib/feature/home/view/pages/home_page.dart
+++ client/lib/feature/home/view/pages/home_page.dart
@@ -1,14 +1,41 @@
-import 'package:client/core/provider/current_user_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final user = ref.watch(currentUserNotifierProvider);
-    print(user);
-    return const Scaffold();
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  int selectedindex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedindex,
+        onTap: (value) {
+          setState(() {
+            selectedindex = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/images/home_filled.png',
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/images/library.png',
+            ),
+            label: 'Library',
+          ),
+        ],
+      ),
+    );
   }
 }
