a56322db59b43e13f2ed1c2e584447032bb8d6b1
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-09 12:23:30 +0800

    [123] Update by adding rgbToHex function and token

--- client/lib/feature/home/viewmodel/home_viewmodel.dart
+++ client/lib/feature/home/viewmodel/home_viewmodel.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 import 'dart:ui';
 
+import 'package:client/core/provider/current_user_notifier.dart';
+import 'package:client/core/utils.dart';
 import 'package:client/feature/home/repositories/home_repository.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'home_viewmodel.g.dart';
@@ -23,13 +26,19 @@ class HomeViewmodel extends _$HomeViewmodel {
     required Color selectedColor,
   }) async {
     state = const AsyncValue.loading();
-    final res = _homeRepository.uploadSong(
+    final res = await _homeRepository.uploadSong(
       selectedAudio: selectedAudio,
       selectedThumbnail: selectedThumbnail,
       songName: songName,
       artist: artist,
-      hexCode: hexCode,
-      token: token,
+      hexCode: rgbToHex(selectedColor),
+      token: ref.read(currentUserNotifierProvider)!.token,
     );
+
+    final val = switch (res) {
+      Left(value: final l) => AsyncValue.error(l.message, StackTrace.current),
+      Right(value: final r) => AsyncValue.data(r),
+    };
+    print(val);
   }
 }
