a890e403e59b1b03eea02c42c7533298b1c028de
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-09 16:35:15 +0800

    [136] Add function getAllSongs

--- client/lib/feature/home/viewmodel/home_viewmodel.dart
+++ client/lib/feature/home/viewmodel/home_viewmodel.dart
@@ -3,11 +3,25 @@ import 'dart:ui';
 
 import 'package:client/core/provider/current_user_notifier.dart';
 import 'package:client/core/utils.dart';
+import 'package:client/feature/home/models/song_model.dart';
 import 'package:client/feature/home/repositories/home_repository.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'home_viewmodel.g.dart';
+
+@riverpod
+Future<List<SongModel>> getAllSongs(GetAllSongsRef ref) async {
+  final token = ref.watch(currentUserNotifierProvider)!.token;
+  final res = await ref.watch(homeRepositoryProvider).getAllSongs(
+        token: token,
+      );
+
+  return switch (res) {
+    Left(value: final l) => throw l.message,
+    Right(value: final r) => r,
+  };
+}
 
 @riverpod
 class HomeViewmodel extends _$HomeViewmodel {
