d19b99b57b113b0825219e4eac33fd24fbfc502f
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-09 12:02:56 +0800

    [121] Update required fields

--- client/lib/feature/home/repositories/home_repository.g.dart
+++ client/lib/feature/home/repositories/home_repository.g.dart
@@ -1,26 +1,25 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'home_viewmodel.dart';
+part of 'home_repository.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$homeViewmodelHash() => r'8bbc226244b517b2fa913cec8355da8344766d32';
+String _$homeRepositoryHash() => r'0a08542f3cf9d0425f87a20b06020cdd03fa827f';
 
-/// See also [HomeViewmodel].
-@ProviderFor(HomeViewmodel)
-final homeViewmodelProvider =
-    AutoDisposeNotifierProvider<HomeViewmodel, AsyncValue?>.internal(
-  HomeViewmodel.new,
-  name: r'homeViewmodelProvider',
+/// See also [homeRepository].
+@ProviderFor(homeRepository)
+final homeRepositoryProvider = AutoDisposeProvider<HomeRepository>.internal(
+  homeRepository,
+  name: r'homeRepositoryProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$homeViewmodelHash,
+      : _$homeRepositoryHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$HomeViewmodel = AutoDisposeNotifier<AsyncValue?>;
+typedef HomeRepositoryRef = AutoDisposeProviderRef<HomeRepository>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- client/lib/feature/home/viewmodel/home_viewmodel.dart
+++ client/lib/feature/home/viewmodel/home_viewmodel.dart
@@ -1,11 +1,35 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:client/feature/home/repositories/home_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'home_viewmodel.g.dart';
 
 @riverpod
 class HomeViewmodel extends _$HomeViewmodel {
+  late HomeRepository _homeRepository;
   @override
   AsyncValue? build() {
+    _homeRepository = ref.watch(homeRepositoryProvider);
     return null;
+  }
+
+  Future<void> uploadSong({
+    required File selectedAudio,
+    required File selectedThumbnail,
+    required String songName,
+    required String artist,
+    required Color selectedColor,
+  }) async {
+    state = const AsyncValue.loading();
+    final res = _homeRepository.uploadSong(
+      selectedAudio: selectedAudio,
+      selectedThumbnail: selectedThumbnail,
+      songName: songName,
+      artist: artist,
+      hexCode: hexCode,
+      token: token,
+    );
   }
 }
