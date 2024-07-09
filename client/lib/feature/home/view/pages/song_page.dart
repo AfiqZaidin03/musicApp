8cbfa3d0cf0b7a945e0e783083a89ec95ff8ec3f
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-09 17:04:08 +0800

    [137] Add safeare and some styling to songs image

--- client/lib/feature/home/view/pages/song_page.dart
+++ client/lib/feature/home/view/pages/song_page.dart
@@ -1,3 +1,5 @@
+import 'package:client/core/widgets/loader.dart';
+import 'package:client/feature/home/viewmodel/home_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +8,63 @@ class SongPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Latest today',
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          ref.watch(getAllSongsProvider).when(
+                data: (songs) {
+                  print(songs);
+                  return SizedBox(
+                    height: 260,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: songs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 16.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 180,
+                                height: 180,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      songs[index].thumbnail_url,
+                                    ),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+                error: (error, st) {
+                  return Center(
+                    child: Text(
+                      error.toString(),
+                    ),
+                  );
+                },
+                loading: () => const Loader(),
+              ),
+        ],
+      ),
+    );
   }
 }
