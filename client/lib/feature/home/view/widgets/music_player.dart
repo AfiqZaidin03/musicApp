0acd3b9db2d093317c6f1ce8c7157ae67e355cef
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-10 14:47:50 +0800

    [148] Create music player page and navigate from music slab

---
+++ client/lib/feature/home/view/widgets/music_player.dart
@@ -0,0 +1,30 @@
+import 'package:client/core/provider/current_song_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MusicPlayer extends ConsumerWidget {
+  const MusicPlayer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentSong = ref.watch(currentSongNotifierProvider);
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  currentSong!.thumbnail_url,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
