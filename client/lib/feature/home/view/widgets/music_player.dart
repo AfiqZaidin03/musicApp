e1c342f8fcfccbbe7534cdb8cbc6b49cbb9dd2ae
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-10 14:58:09 +0800

    [149] Add song name, artist and save button with heart shape

--- client/lib/feature/home/view/widgets/music_player.dart
+++ client/lib/feature/home/view/widgets/music_player.dart
@@ -1,4 +1,6 @@
 import 'package:client/core/provider/current_song_notifier.dart';
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,13 +16,56 @@ class MusicPlayer extends ConsumerWidget {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                  currentSong!.thumbnail_url,
+          Expanded(
+            flex: 5,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    currentSong!.thumbnail_url,
+                  ),
+                  fit: BoxFit.cover,
                 ),
               ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          currentSong.song_name,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                            color: Pallete.whiteColor,
+                          ),
+                        ),
+                        Text(
+                          currentSong.artist,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Pallete.subtitleText,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Expanded(child: SizedBox()),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        CupertinoIcons.heart,
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
           )
         ],
