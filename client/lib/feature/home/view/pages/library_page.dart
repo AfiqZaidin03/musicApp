4256ace9e4add878c9212e7f487c147514596f1b
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-12 17:42:46 +0800

    [193] Update by adding page route to upload song and play song in library list

--- client/lib/feature/home/view/pages/library_page.dart
+++ client/lib/feature/home/view/pages/library_page.dart
@@ -1,6 +1,9 @@
+import 'package:client/core/provider/current_song_notifier.dart';
 import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/core/widgets/loader.dart';
+import 'package:client/feature/home/view/pages/upload_song_page.dart';
 import 'package:client/feature/home/viewmodel/home_viewmodel.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,9 +17,38 @@ class LibraryPage extends ConsumerWidget {
             return ListView.builder(
               itemCount: data.length + 1,
               itemBuilder: (context, index) {
+                if (index == data.length) {
+                  return ListTile(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const UploadSongPage(),
+                        ),
+                      );
+                    },
+                    leading: const CircleAvatar(
+                      radius: 35,
+                      backgroundColor: Pallete.backgroundColor,
+                      child: Icon(CupertinoIcons.plus),
+                    ),
+                    title: const Text(
+                      'Upload New Song',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  );
+                }
+
                 final song = data[index];
 
                 return ListTile(
+                  onTap: () {
+                    ref
+                        .read(currentSongNotifierProvider.notifier)
+                        .updateSong(song);
+                  },
                   leading: CircleAvatar(
                     backgroundImage: NetworkImage(song.thumbnail_url),
                     radius: 35,
