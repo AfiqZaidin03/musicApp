ccf61b48ec3d85a297096f202b73b933eb1d2ba5
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-15 16:20:28 +0800

    [198] Create function for update the real time favorite song to reflect icon and favorite list

--- client/lib/feature/home/viewmodel/home_viewmodel.dart
+++ client/lib/feature/home/viewmodel/home_viewmodel.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 
 import 'package:client/core/provider/current_user_notifier.dart';
 import 'package:client/core/utils.dart';
+import 'package:client/feature/home/models/fav_song_model.dart';
 import 'package:client/feature/home/models/song_model.dart';
 import 'package:client/feature/home/repositories/home_local_repository.dart';
 import 'package:client/feature/home/repositories/home_repository.dart';
@@ -89,8 +90,39 @@ class HomeViewmodel extends _$HomeViewmodel {
     final val = switch (res) {
       Left(value: final l) => state =
           AsyncValue.error(l.message, StackTrace.current),
-      Right(value: final r) => state = AsyncValue.data(r),
+      Right(value: final r) => _favSongSuccess(r, songId),
     };
     print(val);
+  }
+
+  AsyncValue _favSongSuccess(bool isFavorited, String songId) {
+    final userNotifier = ref.read(currentUserNotifierProvider.notifier);
+    if (isFavorited) {
+      userNotifier.addUser(
+        ref.read(currentUserNotifierProvider)!.copyWith(
+          favorites: [
+            ...ref.read(currentUserNotifierProvider)!.favorites,
+            FavSongModel(
+              id: '',
+              song_id: songId,
+              user_id: '',
+            ),
+          ],
+        ),
+      );
+    } else {
+      userNotifier.addUser(
+        ref.read(currentUserNotifierProvider)!.copyWith(
+              favorites: ref
+                  .read(currentUserNotifierProvider)!
+                  .favorites
+                  .where(
+                    (fav) => fav.song_id != songId,
+                  )
+                  .toList(),
+            ),
+      );
+    }
+    return state = AsyncValue.data(isFavorited);
   }
 }
