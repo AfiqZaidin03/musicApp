21e2ba3a11be98c15913e86e3bc94a4e6d6e647e
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-16 16:47:31 +0800

    [210] Add new container color

--- client/lib/core/theme/app_pallete.dart
+++ client/lib/core/theme/app_pallete.dart
@@ -11,8 +11,10 @@ class Pallete {
   static const Color gradient2 = Color.fromRGBO(251, 109, 169, 1);
   static const Color gradient3 = Color.fromRGBO(255, 159, 124, 1);
   static const Color borderColor = Color.fromRGBO(52, 51, 67, 1);
+  static const Color containerColor = Color.fromRGBO(33, 33, 33, 1);
   static const Color whiteColor = Colors.white;
   static const Color greyColor = Colors.grey;
+
   static const Color errorColor = Colors.redAccent;
   static const Color transparentColor = Colors.transparent;
 
