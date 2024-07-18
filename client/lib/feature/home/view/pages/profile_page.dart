91b023f8ddd6b669ef7eb9d8e4b4d59351214a4d
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-07-18 17:08:35 +0800

    [231] Add null condition if no user data

--- client/lib/feature/home/view/pages/profile_page.dart
+++ client/lib/feature/home/view/pages/profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:client/core/provider/current_user_notifier.dart';
 import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/core/widgets/app_bar.dart';
+import 'package:client/feature/auth/view/pages/login_page.dart';
+import 'package:client/feature/auth/viewmodel/auth_viewmodel.dart';
 import 'package:client/feature/home/view/widgets/profile_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,6 +14,7 @@ class ProfilePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final userData = ref.watch(currentUserNotifierProvider);
+    final userSignOut = ref.watch(authViewModelProvider.notifier);
 
     return Scaffold(
       appBar: BasicAppbar(
@@ -45,19 +48,51 @@ class ProfilePage extends ConsumerWidget {
               ),
             ),
             const SizedBox(height: 24),
-            ProfileContainer(
-              label: 'Username',
-              value: userData!.name,
-              isFirst: true,
-            ),
-            ProfileContainer(
-              label: 'Email',
-              value: userData.email,
-            ),
-            ProfileContainer(
-              label: 'Favorites',
-              value: '${userData.favorites.length}',
-              isLast: true,
+            if (userData != null) ...[
+              ProfileContainer(
+                label: 'Username',
+                value: userData.name,
+                isFirst: true,
+              ),
+              ProfileContainer(
+                label: 'Email',
+                value: userData.email,
+              ),
+              ProfileContainer(
+                label: 'Favorites',
+                value: '${userData.favorites.length}',
+                isLast: true,
+              ),
+            ] else ...[
+              const Center(child: Text('User data not available')),
+            ],
+            const SizedBox(height: 24),
+            Center(
+              child: TextButton(
+                onPressed: () {
+                  userSignOut.signOutUser();
+
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginPage(),
+                    ),
+                    (_) => false,
+                  );
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Pallete.containerColor),
+                ),
+                child: const Text(
+                  'Sign Out',
+                  style: TextStyle(
+                    color: Pallete.greyColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
